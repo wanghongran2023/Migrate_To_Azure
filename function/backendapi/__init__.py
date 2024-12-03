1c97c35494cceba8cca59980e6f7d298df12a6c2
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-04 00:43:29 +0900

    Update __init__.py

--- function/backendapi/__init__.py
+++ function/backendapi/__init__.py
@@ -31,7 +31,7 @@ def main(msg: func.ServiceBusMessage):
 
         subject, message = notification
 
-        cursor.execute("SELECT first_name, email FROM attendees")
+        cursor.execute("SELECT first_name, email FROM attendee")
         attendees = cursor.fetchall()
 
         #for name, email in attendees:
