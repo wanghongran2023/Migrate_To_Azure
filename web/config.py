da0d37f3ddf6904cd89ee4097d0164ff7c5e0da0
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-03 15:28:50 +0900

    Update config.py

--- web/config.py
+++ web/config.py
@@ -4,15 +4,15 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL=${{ vars.DB_SERVER_NAME }}.postgres.database.azure.com"
+    POSTGRES_USER="{tmp_db_server_user}"
+    POSTGRES_PW="{tmp_db_server_password}"
+    POSTGRES_DB="{tmp_db_name}"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='{tmp_service_bus_connection}' 
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
