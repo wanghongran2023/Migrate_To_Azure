54bf4b7da6b768a4c2560be578ed73f66ee0c2b3
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-03 14:11:01 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   tenant_id       = var.provider_credentials.tenant_id
   client_id       = var.provider_credentials.sp_client_id
   client_secret   = var.provider_credentials.sp_client_secret
-  features {}
+  features {} # Required for azurerm 3.x
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -21,40 +21,43 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.resource_group_config.location
 }
 
-
 resource "azurerm_postgresql_server" "dbserver" {
   name                         = var.db_server_config.name
   resource_group_name          = azurerm_resource_group.resource_group.name
   location                     = azurerm_resource_group.resource_group.location
-  version                      = "12.0"
+  version                      = "12"
   administrator_login          = var.db_server_config.user
   administrator_login_password = var.db_server_config.password
+
+  sku_name = "B_Gen5_1"
+  storage_mb             = 5120
+  backup_retention_days  = 7
+  geo_redundant_backup_enabled = false
+  ssl_enforcement_enabled = true
 
   depends_on = [azurerm_resource_group.resource_group]
 }
 
 resource "azurerm_postgresql_database" "db" {
   name                = var.db_config.name
-  server_id           = azurerm_postgresql_server.dbserver.id
-  sku_name            = "Basic"
-  collation           = "SQL_Latin1_General_CP1_CI_AS"
-  max_size_gb         = 2
-  min_capacity        = 0.5
-  zone_redundant      = false
-  auto_pause_delay_in_minutes = 60
-  depends_on = [azurerm_postgresql_server.dbserver]
+  resource_group_name = azurerm_resource_group.resource_group.name
+  server_name         = azurerm_postgresql_server.dbserver.name
+  charset             = "UTF8" # Preferred charset for PostgreSQL
+  collation           = "English_United States.1252" # PostgreSQL collation
 }
 
 resource "azurerm_postgresql_firewall_rule" "allow_azure_ips" {
-  name                = "AllowAzureServices"
-  server_id           = azurerm_postgresql_server.dbserver.id
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  name       = "AllowAzureServices"
+  server_name = azurerm_postgresql_server.dbserver.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 resource "azurerm_postgresql_firewall_rule" "allow_all_ips" {
-  name                = "AllowAllIPs"
-  server_id           = azurerm_postgresql_server.dbserver.id
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
+  name       = "AllowAllIPs"
+  server_name = azurerm_postgresql_server.dbserver.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
 }
