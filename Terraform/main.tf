9ceef5555e54ce78113016ab91ff53b6a8e45bed
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-03 15:18:20 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -90,3 +90,24 @@ resource "azurerm_linux_web_app" "linux_webapp" {
   app_settings = { 
   }
 }
+
+resource "azurerm_servicebus_namespace" "servicebus" {
+  name                = var.servicebus_config.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_namespace_authorization_rule" "servicebus_authorization_rule" {
+  name                = "RootManageSharedAccessKey"
+  namespace_name      = azurerm_servicebus_namespace.servicebus.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  listen              = true
+  send                = true
+  manage              = true
+}
+
+output "service_bus_connection_string" {
+  value = azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule.primary_connection_string
+  sensitive = true
+}
